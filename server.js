6e8fe50967bb294759dfc70d390924c31afd01c9
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2024-05-09 14:57:21 +0300

    Initial commit

--- server.js
+++ server.js
@@ -15,14 +15,26 @@ let notes = [
     { id: 5, title: 'Note Five', content: 'Note 5 Content goes here'},
 ];
 
+//Get Starred Notes
+//app.get('/api/starred-notes/', (req, res) => {
+//    const limit = parseInt(req.query.limit);
+
+//    if (!isNaN(limit) && limit > 0) {
+//        res.json(notes.slice(0, limit));
+//    } else {
+//        res.json(notes);
+//    }
+//});
+
 //Get all notes
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    res.status(200).json(notes);
 });
 
 //Get a single note
 app.get('/api/notes/:id', (req, res) => {
-    res.json(notes);
+    const id = parseInt(req.params.id);
+    res.status(200).json(notes.filter((note) => note.id === id));
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
